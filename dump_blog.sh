74ec50d3724799762bea0156d5fc2d2be58c2954
Marko Samastur <markos@gaivo.net>
2015-05-07 09:10:06 +0200

    Script for fetching and processing blog posts

---
+++ dump_blog.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd dump
+#wget -r -l 115 http://markos.gaivo.net
+rm -rf posts
+mkdir posts
+for i in markos.gaivo.net/blog/index.html?p=*; { cp $i "posts/"`echo $i|awk -F"p=" '{print $2}'`".html"; }
