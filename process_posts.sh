0bafffaeb91a3d60ac5b4682172ed875d2dae34e
Marko Samastur <markos@gaivo.net>
2015-11-07 19:55:05 +0100

    Renamed website to articles

--- process_posts.sh
+++ process_posts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wp2pelican --wpfile -o old_posts wordpress.2015-03-02.xml --wpposts dump/posts/ -m markdown
-mv old_posts/* website/content/
+mv old_posts/* articles/content/
 rmdir old_posts
